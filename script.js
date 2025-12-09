22172470b2cc42b1c65c7b5a71d86b835910546c
Loe <39932664+TallChain@users.noreply.github.com>
2025-12-08 19:02:42 -0800

    Feat: Adding basic operations

---
+++ script.js
@@ -0,0 +1,36 @@
+//Calculations
+let int1
+let int2
+let operator
+function add(a, b) {
+    console.log(a+b)
+}
+function subtract(a, b) {
+    console.log(a-b)
+}
+function multiply(a, b) {
+    console.log(a*b)
+}
+function divide(a, b) {
+    console.log(a/b)
+}
+function operate(int1, int2, op){
+    if (op == "+") {add(int1,int2)}
+    else if (op == "-") {subtract(int1,int2)}
+    else if (op == "*") {multiply(int1, int2)}
+    else if (op == "/") {divide(int1,int2)}
+    else return("error")
+}
+
+//Manipulating DOM
+let container = document.querySelector("div")
+let selectButton = document.querySelector(".buttons")
+
+//Making buttons
+for (let makeButtonNum = 0; makeButtonNum < 10; makeButtonNum++) {
+    let createButton = document.createElement("button")
+    createButton.textContent = makeButtonNum;
+    createButton.classList.add("buttons");
+    container.appendChild(createButton);
+}
+
