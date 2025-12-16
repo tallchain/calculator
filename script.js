093447e2a624c5267bfa197cff0a3534f93b8df2
Loe <39932664+TallChain@users.noreply.github.com>
2025-12-16 12:53:09 -0800

    Refactor: Separate button event listener

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 //Calculations
-let int1 = undefined
-let int2= undefined
-let operator = undefined
+let int1 = undefined;
+let int2= undefined;
+let operator = undefined;
 
 function displayOperationToCalcDisplay (number) {
-    calcDisplay.textContent = Math.round((number + Number.EPSILON) * 100) / 100
-    assignIntFromCalcDisplay()
+    calcDisplay.textContent = Math.round((number + Number.EPSILON) * 100) / 100;
+    assignIntFromCalcDisplay();
 }
 function add(a, b) {
     let number = (parseFloat(a) + parseFloat(b));
-    displayOperationToCalcDisplay(number)
+    displayOperationToCalcDisplay(number);
 }
 function subtract(a, b) {
-    let number = (a - b)
-    displayOperationToCalcDisplay(number)
+    let number = (a - b);
+    displayOperationToCalcDisplay(number);
 
 }
 function multiply(a, b) {
-   let number = (a * b)
-    displayOperationToCalcDisplay(number)
+   let number = (a * b);
+    displayOperationToCalcDisplay(number);
 
 }
 function divide(a, b) {
     if (b == 0) {calcDisplay.textContent = "You can't divide by 0, silly goose."}
     else {let number = (a / b);
-    displayOperationToCalcDisplay(number)
+    displayOperationToCalcDisplay(number);
     }
 }
 function assignIntFromCalcDisplay() {int1 = calcDisplay.textContent}
@@ -38,21 +38,28 @@ function findOperation(operator) {
 }
 
 //Manipulating DOM
-const container = document.querySelector(".container")
-const selectButton = document.querySelector(".button")
-const calcDisplay = document.querySelector(".calcDisplay")
-const clear = document.querySelector(".clear")
-const equalSign = document.querySelector(".equalSign")
-const numberButtonContainer = document.querySelector(".numberButtonContainer")
+const container = document.querySelector(".container");
+const clear = document.querySelector(".clear");
+const equalSign = document.querySelector(".equalSign");
+const calcDisplay = document.querySelector(".calcDisplay");
+const numberButtonContainer = document.querySelector(".numberButtonContainer");
 
 //Creating buttons
 const buttonText = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ".", "+", '-', '*', '/']
-buttonText.forEach((clickedButton) => {
+buttonText.forEach(createButtons);
+
+function createButtons(clickedButton) { 
     const createButton = document.createElement("button")
     createButton.textContent = clickedButton;
     createButton.classList.add("button");
-    createButton.addEventListener("click", () => {
-    if (clickedButton == "+" || clickedButton == "-" || clickedButton == "*" || clickedButton == "/")
+    createButton.addEventListener("click", (e) => addButtonEventListener(e.currentTarget.textContent))
+    numberButtonContainer.appendChild(createButton);}
+
+function addButtonEventListener(clickedButton) { 
+        if (clickedButton == "+" 
+        || clickedButton == "-" 
+        || clickedButton == "*" 
+        || clickedButton == "/")
         if (operator == undefined) {
             int1 = calcDisplay.textContent;
             operator = clickedButton;
@@ -61,23 +68,22 @@ buttonText.forEach((clickedButton) => {
             removeInt2AfterOperation();
             operator = clickedButton;
         }
-    else if (clickedButton == "." && calcDisplay.textContent.includes(".")) {
+    else if (clickedButton == "." 
+        && calcDisplay.textContent.includes(".")) {
         return
     }
-    else if (int1 != undefined && int2 == undefined) { 
+    else if (int1 != undefined
+        && int2 == undefined) { 
         calcDisplay.textContent = clickedButton;
         int2 = calcDisplay.textContent;
     }
     else if (int2 != undefined) {
         calcDisplay.textContent += clickedButton;
-        int2 = calcDisplay.textContent
+        int2 = calcDisplay.textContent;
     }
-    else {calcDisplay.textContent += clickedButton}
-    })
-    numberButtonContainer.appendChild(createButton);
-})
-
-clear.addEventListener("click", () => {calcDisplay.textContent = ""; 
+    else {calcDisplay.textContent += clickedButton}}
+clear.addEventListener("click", () => {
+    calcDisplay.textContent = ""; 
     operator = undefined; 
     int1 = undefined; 
     int2 = undefined;
@@ -85,13 +91,14 @@ clear.addEventListener("click", () => {calcDisplay.textContent = "";
 equalSign.addEventListener("click", () => {
     removeInt2AfterOperation();
     operator = undefined;
-    })
-document.addEventListener("keypress", (e) => { 
-    calcDisplay.textContent += e.key
-    })
+})
+document.addEventListener("keypress", (e) => {
+    if (buttonText.includes(e.key))
+        {calcDisplay.textContent += e.key}
+})
 function removeInt2AfterOperation() {    
         findOperation(operator);
-        int2 = undefined
+        int2 = undefined;
     }
 
     
